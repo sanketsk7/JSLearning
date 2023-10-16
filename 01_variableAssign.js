89ab23a9627479092ca02245f03f3109ec788f0e
sanketsk7 <sanketkadam9790@gmail.com>
2023-10-16 17:37:36 +0530

    Designed VariableAssign
    Designed VariableAssign

---
+++ 01_variableAssign.js
@@ -0,0 +1,33 @@
+var myFullName;
+console.log(myFullName);
+
+var myFullName = "Sanket Kadam";
+
+console.log(myFullName);
+
+var myFullName = "Sanket Shamrao Kadam";
+console.log(myFullName);
+
+var myLove = "I love only js";
+console.log(myLove);
+
+var firstName = "Sanket";
+console.log("First Name:", firstName);
+
+var lastName = "Kadam";
+console.log("Last Name:", lastName);
+
+var age = "25";
+console.log("Age:", age);
+
+var address = "Pune";
+console.log("Address:", address);
+
+var pinCode = "416302";
+console.log("PinCode:", pinCode);
+
+var gender = "Male";
+console.log("Gender:", gender);
+
+var isMarried = "No";
+console.log("Is Married:", isMarried);
