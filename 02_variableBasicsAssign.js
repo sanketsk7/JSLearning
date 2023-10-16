21ad0ef1a0ba7ad6010ffc5f6ae7bd54dd777118
sanketsk7 <sanketkadam9790@gmail.com>
2023-10-16 21:14:09 +0530

    Variable basics Assign
    Variable basics Assign

---
+++ 02_variableBasicsAssign.js
@@ -0,0 +1,34 @@
+var mobile = 9890527825;
+console.log("Mobile:", mobile, "Data type:", typeof mobile);
+
+var pinCode = 416302;
+console.log("PinCode:", pinCode, "Data type:", typeof pinCode);
+
+var bike = 1234;
+console.log("Bike:", bike, "Data type", typeof bike);
+
+var marks = "81%";
+console.log("HSC Marks", marks, "Data type", typeof marks);
+
+var player = "MSD";
+console.log(" Favorite Player", player, "Data type", typeof player);
+
+var hobbies = "Coding%";
+console.log("Hobbies", hobbies, "Data type", typeof hobbies);
+
+var isFamous = false;
+console.log("Famous", isFamous, "Data type", typeof isFamous);
+
+var isGraduation = true;
+console.log("Education", isGraduation, "Data type", typeof isGraduation);
+
+var isPlace = true;
+console.log("City ", isPlace, "Data type", typeof isPlace);
+
+console.log("--------------null vs undefined -----------------");
+
+var email;
+console.log(email);
+
+email = null;
+console.log(email);
