a1a08129907a2b5d9690cbb75da53a7925688287
sanketsk7 <sanketkadam9790@gmail.com>
2023-10-17 15:04:35 +0530

    Designed Type of JS
    Designed Type of JS

---
+++ 02_typeof.js
@@ -0,0 +1,47 @@
+console.log("----------Assignment No 2------------");
+console.log("-------------Step 1----------------- ");
+
+var bankName;
+console.log(
+  "Value before initialization:",
+  bankName,
+  "Type of:",
+  typeof bankName
+);
+bankName = "SBI BANK";
+console.log(
+  "Value after initialization:",
+  bankName,
+  "Type of:",
+  typeof bankName
+);
+
+console.log("-------------Step 2----------------- ");
+
+marks = "90%";
+console.log("Marks:", marks, "Type of:", typeof marks);
+
+console.log("-------------Step 3----------------- ");
+
+var isWorking;
+console.log(
+  "Value before initialization:",
+  isWorking,
+  "Type of:",
+  typeof isWorking
+);
+isWorking = true;
+console.log(
+  "Value after initialization:",
+  isWorking,
+  "Type of:",
+  typeof isWorking
+);
+
+console.log("-------------Step 4----------------- ");
+
+var totalCount = "Hundred and Seven";
+console.log("Total Count", totalCount, "Type of:", typeof totalCount);
+
+totalCount = 107;
+console.log("Total Count:", totalCount, "Type of:", typeof totalCount);
