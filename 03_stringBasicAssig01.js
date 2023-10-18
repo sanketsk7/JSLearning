5f9efb2cadb4ac93649d2fbca7fc774664b35192
sanketsk7 <sanketkadam9790@gmail.com>
2023-10-18 16:29:26 +0530

    Assignment 3 Basic String
    Assignment 3 Basic String

---
+++ 03_stringBasicAssig01.js
@@ -0,0 +1,25 @@
+function stringBasics() {
+  console.log("My Dream company Name: TCS");
+}
+stringBasics();
+
+var hobby01 = "coding";
+
+console.log("Hobby 01:", hobby01);
+
+var hobby02 = "traveling";
+
+console.log("Hobby 02:", hobby02);
+
+var hobby03 = "reading";
+
+console.log("Hobby 03:", hobby03);
+
+var totalLength = hobby01.length;
+console.log("Length of Hobby 01:", totalLength);
+
+var totalLength = hobby02.length;
+console.log("Length of Hobby 02:", totalLength);
+
+var totalLength = hobby03.length;
+console.log("Length of Hobby 03:", totalLength);
