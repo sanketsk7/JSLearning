2b568b13ebe07d1acf640050f268d1ff2d53df1c
sanketsk7 <sanketkadam9790@gmail.com>
2023-10-19 15:51:19 +0530

    Updates new char in String
    Updates new char in String

--- 04_stringBasics.js
+++ 04_stringBasics.js
@@ -1,14 +1,10 @@
-var greet = 'Good Morning';
-
- 
+var greet = "Good Morning";
 
 console.log("========= length property =============");
 
 var totalLength = greet.length;
 
-console.log('Length of the string is: ', totalLength);
-
- 
+console.log("Length of the string is: ", totalLength);
 
 console.log("========== charAt() =============== ");
 
@@ -17,49 +13,41 @@ var charAt0 = greet.charAt(0);
 console.log("Character at index 0 is: ", charAt0);
 
 var charAt5 = greet.charAt(5);
-console.log("Character at index 5 is:",charAt5);
+console.log("Character at index 5 is:", charAt5);
 
-console.log('Char stored at index 5 is: ', greet.charAt(5));
+console.log("Char stored at index 5 is: ", greet.charAt(5));
 
 console.log("========== concatenation =============== ");
 
 var firstName = "Jenny";
 
-var lastName = " Gates"
+var lastName = " Gates";
 
 var result = firstName.concat(lastName);
 
 console.log(result);
 
- 
-
 console.log("========== indexOf =============== ");
 
-var indexOfM = greet.indexOf('M');
+var indexOfM = greet.indexOf("M");
 
-console.log('Index of M is: ', indexOfM);
+console.log("Index of M is: ", indexOfM);
 
- 
+console.log(greet.indexOf("nin"));
 
- console.log(greet.indexOf('nin'));
+console.log("Index of char z:", greet.indexOf("z"));
 
- 
+console.log("Index of char D:", greet.indexOf("D"));
 
- console.log('Index of char z:',greet.indexOf('z'));
+console.log("Index of char o is:", greet.indexOf("o"));
 
- 
+console.log("========== replace =============== ");
 
- console.log('Index of char D:',greet.indexOf('D'));
-
- console.log('Index of char o is:', greet.indexOf('o'));
-
- console.log("========== replace =============== ");
-
-var result =greet.replace('Morning', "Afternoon");
+var result = greet.replace("Morning", "Afternoon");
 
 console.log(result);
 
-var result = greet.replace(' ', "-");
+var result = greet.replace(" ", "-");
 
 console.log(result);
 
@@ -79,15 +67,83 @@ var lengthAfterTrim = result.length;
 
 var result = lengthBeforeTrim - lengthAfterTrim;
 
-console.log('Removed extra spaces are: ', result);
+console.log("Removed extra spaces are: ", result);
 
+var lastIndexChar = greet.charAt(greet.length - 1);
 
+console.log("Last index char is: ", lastIndexChar);
 
- 
+var lastIndexChar = greet.charAt(greet.length - 3);
+console.log("Last index char is:", lastIndexChar);
 
-var lastIndexChar = greet.charAt(greet.length-1);
+console.log("========== includes() =============== ");
 
-console.log('Last index char is: ', lastIndexChar);
+var result = greet.includes("Morning");
 
-var lastIndexChar = greet.charAt(greet.length-3);
-console.log('Last index char is:',lastIndexChar);
+console.log("Is Morning word available: ", result);
+
+var result = greet.includes("nin");
+
+console.log("Is nin word available: ", result);
+
+var result = greet.includes("good");
+
+console.log("Is good word available: ", result);
+
+console.log("========== search() =============== ");
+
+var greet = "Good Morning";
+
+var result = greet.search("orn");
+
+console.log(result);
+
+var result = greet.search("after");
+
+console.log(result);
+
+console.log("========== slice() =============== ");
+
+var greet = "Hello Good Morning!";
+
+var result = greet.slice(6);
+
+console.log(result);
+
+var result = greet.slice(6, 10);
+
+console.log(result);
+
+console.log("========== split() =============== ");
+
+var result = greet.split(" ");
+
+console.log(result);
+
+//WAP to count the total number words
+
+var str = "I am UI Developer";
+
+var words = str.split(" ");
+
+console.log(words);
+
+console.log("Total words is: ", words.length);
+
+// console.log(typeof words);
+
+console.log(`Total word is :${words.length}`);
+
+// String template
+
+console.log(`I am "UI" Developer `);
+
+var num1 = 10;
+
+var num2 = 20;
+
+var result = num1 + num2;
+
+console.log("Addition of ", num1, "and ", num2, "is :", result);
+
+console.log(` Addition of ${num1} and ${num2}  is: ${result}`);
